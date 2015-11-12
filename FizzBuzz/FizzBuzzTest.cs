fb914045e3fb0604bfc99edaa2a09df8fa4500e9
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-12 15:06:13 +0000

    First commit

---
+++ FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace FizzBuzz
+{
+	[TestFixture ()]
+	public class FizzBuzzTest
+	{
+		FizzBuzz fb;
+
+		[SetUp]
+		public void FizzBuzzCreation ()
+		{
+			fb = new FizzBuzz ();
+		}
+
+		[Test ()]
+		public void Print_Number_If_Not_Multiple_Of_Three_Or_Five ()
+		{
+			Assert.AreEqual (fb.Check ("7"), "7");
+		}
+
+		[Test ()]
+		public void Print_Fizz_For_Multiple_Of_Three ()
+		{
+			Assert.AreEqual (fb.Check ("12"), "fizz");
+		}
+
+		[Test ()]
+		public void Print_Buzz_For_Multiple_Of_Five ()
+		{
+			Assert.AreEqual (fb.Check ("20"), "buzz");
+		}
+
+		[Test ()]
+		public void Print_FizzBuzz_For_Multiple_Of_Three_And_Five ()
+		{
+			Assert.AreEqual (fb.Check ("30"), "fizzbuzz");
+		}
+	}
+}
+
+
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	class FizzBuzz
+	{
+		public string Check (string number) {
+			int user_number = Int32.Parse(number);
+
+			if (user_number % 15 == 0) 
+			{
+				return "fizzbuzz";
+			}
+			else if (user_number % 3 == 0) 
+			{
+				return "fizz";
+			}
+			else if (user_number % 5 == 0) 
+			{
+				return "buzz";
+			}
+			else 
+			{
+				return number;
+			}
+		}
+
+		public static void Main ()
+		{
+			FizzBuzz fb = new FizzBuzz ();
+			Console.WriteLine ("Enter a number: ");
+			string input = Console.ReadLine ();
+			string num = fb.Check (input);
+			Console.WriteLine ("Your number is {0}", num);
+		}
+	}
+}
